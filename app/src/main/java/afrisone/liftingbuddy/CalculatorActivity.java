f64609198240c96195a416d915805e85e9b5d2c5
Alicia Frisone <a.frisone54@gmail.com>
2015-07-25 21:11:21 -0600

    Cleaned up functions in CalculatorActivity.java

--- app/src/main/java/afrisone/liftingbuddy/CalculatorActivity.java
+++ app/src/main/java/afrisone/liftingbuddy/CalculatorActivity.java
@@ -36,14 +36,7 @@ public class CalculatorActivity extends ActionBarActivity {
 
         populateActivitySpinner();
         setUnitsUp();
-
-        //Submit button after user enters age, height, weight, gender, and activity level
-        Button calculatorSubmit = (Button) findViewById(R.id.button_calculate);
-        calculatorSubmit.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                calculateTDEE();
-            }
-        });
+        setUpSubmitButton();
     }
 
     //@Override
@@ -68,15 +61,6 @@ public class CalculatorActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    //Method to determine if metric is selected. Imperial units by default
-    private void getUnits() {
-        //TODO: if metric, change height and weight units to cm and kg
-        if (((RadioButton) findViewById(R.id.metric_units)).isChecked()) {
-            units = "Metric";
-        } else {
-            units = "Imperial";
-        }
-    }
 
     //Change the weight and height from imperial to metric units using the radio buttons
     private void setUnitsUp(){
@@ -84,22 +68,89 @@ public class CalculatorActivity extends ActionBarActivity {
         unitChange.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
-                TextView height = (TextView) findViewById(R.id.height);
-                TextView weight = (TextView) findViewById(R.id.weight);
-                EditText hideInchesField = (EditText) findViewById(R.id.height_in_inches);
-
-                if(checkedId == R.id.metric_units){
-                    hideInchesField.setVisibility(View.GONE);
-                    height.setText("Height (cm)");
-                    weight.setText("Weight (kg)");
-                }
-                else{
-                    hideInchesField.setVisibility(View.VISIBLE);
-                    height.setText("Height (ft/in)");
-                    weight.setText("Weight (lbs)");
+                if (checkedId == R.id.metric_units) {
+                    metricUnits();
+                } else {
+                    imperialUnits();
                 }
             }
         });
+    }
+
+    //Set weight and height fields to metric units
+    private void metricUnits(){
+        TextView height = (TextView) findViewById(R.id.height);
+        TextView weight = (TextView) findViewById(R.id.weight);
+        EditText hideInchesField = (EditText) findViewById(R.id.height_in_inches);
+
+        hideInchesField.setVisibility(View.GONE);
+        height.setText("Height (cm)");
+        weight.setText("Weight (kg)");
+    }
+
+    //Set weight and height fields to imperial units
+    private void imperialUnits(){
+        TextView height = (TextView) findViewById(R.id.height);
+        TextView weight = (TextView) findViewById(R.id.weight);
+        EditText hideInchesField = (EditText) findViewById(R.id.height_in_inches);
+
+        hideInchesField.setVisibility(View.VISIBLE);
+        height.setText("Height (ft/in)");
+        weight.setText("Weight (lbs)");
+    }
+
+    //Method to set up the submit button
+    private void setUpSubmitButton(){
+        Button calculatorSubmit = (Button) findViewById(R.id.button_calculate);
+        calculatorSubmit.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                calculateTDEE();
+            }
+        });
+    }
+
+    //Method to populate the spinner to set the activity level
+    private void populateActivitySpinner(){
+        Spinner activityLevel = (Spinner) findViewById(R.id.activity_level);
+        String[] activityLevels = new String[]{"Sedentary", "Light activity", "Moderate activity", "Very active"};
+        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, activityLevels);
+        activityLevel.setAdapter(spinnerAdapter);
+    }
+
+    //Method to calculate Total Daily Energy Expenditure (Calories to maintain weight)
+    private void calculateTDEE() {
+        calculateREE();
+        TDEE = REE * activityLevel;
+    }
+
+    //Method to calculate the Resting Energy Expenditure
+    private void calculateREE() {
+        setValues();
+
+        if (gender.equals("Female")) {
+            REE = (10 * weight) + (6.25 * height) - (5 * age) - 161;
+        } else {
+            REE = (10 * weight) + (6.25 * height) - (5 * age) + 5;
+        }
+    }
+
+    //Method to call all of the getter methods
+    private void setValues() {
+        getUnits();
+        getGender();
+        getHeight();
+        getAge();
+        getWeight();
+        getActivityLevel();
+    }
+
+    //Method to determine if metric is selected. Imperial units by default
+    private void getUnits() {
+        if (((RadioButton) findViewById(R.id.metric_units)).isChecked()) {
+            units = "Metric";
+        } else {
+            units = "Imperial";
+        }
     }
 
     //Method to set the member variable gender based on user input
@@ -131,13 +182,6 @@ public class CalculatorActivity extends ActionBarActivity {
         }
     }
 
-    //Method to get user input for age
-    private void getAge() {
-        EditText mEdit;
-        mEdit = (EditText) findViewById(R.id.age_in_years);
-        age = Double.parseDouble(mEdit.getText().toString());
-    }
-
     //Method to get user input for weight
     private void getWeight() {
         EditText mEdit;
@@ -150,13 +194,11 @@ public class CalculatorActivity extends ActionBarActivity {
             weight = kilograms;
         }
     }
-
-    //Method to populate the spinner to set the activity level
-    private void populateActivitySpinner(){
-        Spinner activityLevel = (Spinner) findViewById(R.id.activity_level);
-        String[] activityLevels = new String[]{"Sedentary", "Light activity", "Moderate activity", "Very active"};
-        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, activityLevels);
-        activityLevel.setAdapter(spinnerAdapter);
+    //Method to get user input for age
+    private void getAge() {
+        EditText mEdit;
+        mEdit = (EditText) findViewById(R.id.age_in_years);
+        age = Double.parseDouble(mEdit.getText().toString());
     }
 
     //Method to get user input for the activity level
@@ -178,33 +220,6 @@ public class CalculatorActivity extends ActionBarActivity {
                 activityLevel = 1.725;
                 break;
         }
-    }
-
-    //Method to call all of the getter methods
-    private void setValues() {
-        getUnits();
-        getGender();
-        getHeight();
-        getAge();
-        getWeight();
-        getActivityLevel();
-    }
-
-    //Method to calculate the Resting Energy Expenditure
-    private void calculateREE() {
-        setValues();
-
-        if (gender.equals("Female")) {
-            REE = (10 * weight) + (6.25 * height) - (5 * age) - 161;
-        } else {
-            REE = (10 * weight) + (6.25 * height) - (5 * age) + 5;
-        }
-    }
-
-    //Method to calculate Total Daily Energy Expenditure (Calories to maintain weight)
-    private void calculateTDEE() {
-        calculateREE();
-        TDEE = REE * activityLevel;
     }
 }
 
