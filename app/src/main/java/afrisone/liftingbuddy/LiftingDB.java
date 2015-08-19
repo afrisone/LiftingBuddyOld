0abda2a37c5462e815d31c4e8ea5161369b85e60
Alicia Frisone <a.frisone54@gmail.com>
2015-08-19 16:12:06 -0600

    Added initial database class - in progress

---
+++ app/src/main/java/afrisone/liftingbuddy/LiftingDB.java
@@ -0,0 +1,22 @@
+package afrisone.liftingbuddy;
+
+
+public final class LiftingDB {
+    private int caloriesDB;
+    private int proteinDB;
+    private int fatDB;
+    private int carbohydratesDB;
+
+    LiftingDB() {}
+
+    LiftingDB(int calories, int protein, int fat, int carbohydrates) {
+        caloriesDB = calories;
+        proteinDB = protein;
+        fatDB = fat;
+        carbohydratesDB = carbohydrates;
+    }
+
+
+
+}
+
