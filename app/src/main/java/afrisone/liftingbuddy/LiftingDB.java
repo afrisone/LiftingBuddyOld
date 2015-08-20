8afecb4148db73ba94da438c284f19113805c387
Alicia Frisone <a.frisone54@gmail.com>
2015-08-19 21:10:35 -0600

    Continued adding to database classes

--- app/src/main/java/afrisone/liftingbuddy/LiftingDB.java
+++ app/src/main/java/afrisone/liftingbuddy/LiftingDB.java
@@ -1,22 +1,37 @@
 package afrisone.liftingbuddy;
 
 
-public final class LiftingDB {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public final class LiftingDB extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "LiftingDB";
+    private static final int DATABASE_VERSION = 2;
     private int caloriesDB;
     private int proteinDB;
     private int fatDB;
     private int carbohydratesDB;
 
-    LiftingDB() {}
-
-    LiftingDB(int calories, int protein, int fat, int carbohydrates) {
-        caloriesDB = calories;
-        proteinDB = protein;
-        fatDB = fat;
-        carbohydratesDB = carbohydrates;
+    public LiftingDB(Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
 
+    private static final String DATABASE_CREATE = "CREATE TABLE MACROS(calories int primary key,protein int,fat int,carbs int);";
 
+    @Override
+    public void onCreate(SQLiteDatabase database) {
+        database.execSQL(DATABASE_CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase database, int oldVersion, int newVersion) {
+        Log.w(LiftingDB.class.getName(), "Upgrading database from version " + oldVersion +
+                " to " + newVersion + ", which will destroy all old data");
+        database.execSQL("DROP TABLE IF EXISTS MACROS");
+        onCreate(database);
+    }
 }
 
