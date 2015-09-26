61642d0c747ba19edbc933920050d7ffca28295b
Alicia Frisone <a.frisone54@gmail.com>
2015-09-25 22:58:26 -0600

    Initial database commit

--- app/src/main/java/afrisone/liftingbuddy/LiftingDB.java
+++ app/src/main/java/afrisone/liftingbuddy/LiftingDB.java
@@ -4,34 +4,36 @@ package afrisone.liftingbuddy;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.util.Log;
 
 public final class LiftingDB extends SQLiteOpenHelper {
+    private static final int DATABASE_VERSION = 1;
     private static final String DATABASE_NAME = "LiftingDB";
-    private static final int DATABASE_VERSION = 2;
-    private int caloriesDB;
-    private int proteinDB;
-    private int fatDB;
-    private int carbohydratesDB;
+
+    //Macros Table
+    private static final String TABLE_MACROS = "macros";
+    private static final String KEY_CALORIES = "calories";
+    private static final String KEY_PROTEIN = "protein";
+    private static final String KEY_FAT = "fat";
+    private static final String KEY_CARBS = "carbs";
 
     public LiftingDB(Context context){
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
-
-    private static final String DATABASE_CREATE = "CREATE TABLE MACROS(calories int primary key,protein int,fat int,carbs int);";
-
+    //Create the Macronutrients table in the SQLite database
     @Override
-    public void onCreate(SQLiteDatabase database) {
-        database.execSQL(DATABASE_CREATE);
+    public void onCreate(SQLiteDatabase db){
+        String CREATE_MACROS_TABLE = "CREATE TABLE" + TABLE_MACROS + "(" + KEY_CALORIES
+                + " INTEGER PRIMARY KEY," + KEY_PROTEIN + " INTEGER," + KEY_FAT + " INTEGER"
+                + KEY_CARBS + " INTEGER" + ")";
+        db.execSQL(CREATE_MACROS_TABLE);
     }
 
+    //Create new tables after upgrading
     @Override
-    public void onUpgrade(SQLiteDatabase database, int oldVersion, int newVersion) {
-        Log.w(LiftingDB.class.getName(), "Upgrading database from version " + oldVersion +
-                " to " + newVersion + ", which will destroy all old data");
-        database.execSQL("DROP TABLE IF EXISTS MACROS");
-        onCreate(database);
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_MACROS);
+        onCreate(db);
     }
 }
 
